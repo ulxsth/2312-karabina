c9ed91a395c21966001f1e7479c5d334bcbe36d1
ulxsth <ulxsth@gmail.com>
2023-12-16 02:03:00 +0900

    update: 認証関連のルーティングをauth.phpに移動

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,4 @@
 <?php
-
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,14 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::get('/auth/spotify', function (Request $request) {
-    $authController = new AuthController();
-    $authController->redirectToSpotify();
-});
-
-Route::get('/auth/spotify/callback', function (Request $request) {
-    $authController = new AuthController();
-    $authController->handleSpotifyCallback($request);
 });
