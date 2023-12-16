5d3f4602aaf88f32b54b6c770adc2c0c51956841
kurumayusamugariko <junmomo6780@gmail.com>
2023-12-16 22:23:45 +0900

    ritu>>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="theme-color" content="#000000">
-        <link rel="stylesheet" href="/app.css" />
+        <!-- <link rel="stylesheet" href="/app.css" /> -->
+        <link rel="stylesheet" href="{{ stylesheets('css/app.css') }}">
         <!--googlefont londrinascketch-->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
