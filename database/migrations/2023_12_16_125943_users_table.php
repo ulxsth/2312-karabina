35eecf5ec6a7dad8d9afb996f464c8320637b16b
ulxsth <ulxsth@gmail.com>
2023-12-17 00:10:47 +0900

    update: usersテーブル作成用のmigrationファイルを作成

---
+++ database/migrations/2023_12_16_125943_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('spotify_id')->primary();
+            $table->string('access_token');
+            $table->string('refresh_token');
+            $table->integer('token_updated_at');
+            $table->integer('token_expire');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
