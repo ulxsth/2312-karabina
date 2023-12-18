306ae6ac8b6caf154e1d689c7ba782f01123bf3b
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-12-18 23:28:26 +0900

    change button color

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -8,9 +8,7 @@
 <div class="logo">
     <h1>Recentri <span>for Spotify</span></h1> 
 </div>
-<form method="POST" action="{{ route('spotify/auth') }}" >
     <button>
         <h1>Sign In</h1>
     </button>
-</form>
 @endsection
