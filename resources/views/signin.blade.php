bb80894f31f9466ff24966dfc315aa0cdc400e92
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-12-19 10:53:38 +0900

    ボタンデザインの変更

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -8,7 +8,9 @@
 <div class="logo">
     <h1>Recentri <span>for Spotify</span></h1> 
 </div>
+<form action="{{ route('') }}" method="POST">
     <button>
         <h1>Sign In</h1>
     </button>
+</form>
 @endsection
