685e4fe4046a637f380a0b07d2b9df51bfbea7f2
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-12-22 23:08:41 +0900

    インデント整理

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,34 +14,34 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Lusitana:wght@700&display=swap" rel="stylesheet">
-				<!--googlefont Rubik Broken Fax-->
-				<link rel="preconnect" href="https://fonts.googleapis.com">
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-				<link href="https://fonts.googleapis.com/css2?family=Rubik+Broken+Fax&display=swap" rel="stylesheet">
+		<!--googlefont Rubik Broken Fax-->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Rubik+Broken+Fax&display=swap" rel="stylesheet">
         <!--googlefont Rubik Lines-->
-				<link rel="preconnect" href="https://fonts.googleapis.com">
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-				<link href="https://fonts.googleapis.com/css2?family=Rubik+Lines&display=swap" rel="stylesheet">
-				<title>Recentri</title>
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Rubik+Lines&display=swap" rel="stylesheet">
+		<title>Recentri</title>
 
         @yield('head')
 
     </head>
     <body>
-				<!-- particles.js container -->
-				<div id="particles-js"></div>
-				<!-- stats - count particles -->
-				<div class="count-particles">
-					<span class="js-count-particles"></span>
-				</div>
-					<!-- particles.js lib - https://github.com/VincentGarreau/particles.js -->
-					<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-					<!-- stats.js lib -->
-					<script src="http://threejs.org/examples/js/libs/stats.min.js"></script>
+		<!-- particles.js container -->
+		<div id="particles-js"></div>
+		<!-- stats - count particles -->
+		<div class="count-particles">
+			<span class="js-count-particles"></span>
+		</div>
+		<!-- particles.js lib - https://github.com/VincentGarreau/particles.js -->
+		<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+		<!-- stats.js lib -->
+		<script src="http://threejs.org/examples/js/libs/stats.min.js"></script>
 
-					<div class = "space"></div>
-					<img src="\chara2.png">
-					<script src="{{ asset('js/app.js') }}"></script>
+		<div class = "space"></div>
+		<img src="\chara2.png">
+		<script src="{{ asset('js/app.js') }}"></script>
         @yield('content')
     </body>
 </html>
