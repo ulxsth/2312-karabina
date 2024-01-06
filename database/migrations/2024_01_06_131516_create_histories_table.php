62f98c119370cb469010c8505374a60e7e33a9c8
ulxsth <ulxsth@gmail.com>
2024-01-06 22:21:59 +0900

    update: Historyモデル、historiesテーブルを作成

---
+++ database/migrations/2024_01_06_131516_create_histories_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('histories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('user_spotify_id');
+            $table->string('track_spotify_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('histories');
+    }
+};
