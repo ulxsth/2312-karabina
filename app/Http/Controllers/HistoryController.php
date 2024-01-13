3c73d09c8598bd2365e50538b572f72086688bd1
ulxsth <ulxsth@gmail.com>
2024-01-13 22:46:03 +0900

    fix: 引数をバラバラに受け取るように

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -16,14 +16,16 @@ class HistoryController extends Controller
 
     /**
      * ユーザーの履歴をSpotifyから取得し、DBに保存する。
-     * @param SpotifyUser $user
+     * @param string $spotifyId SpotifyのユーザーID
+     * @param string $accessToken Spotifyのアクセストークン
+     * @param string $refreshToken Spotifyのリフレッシュトークン
      * @return boolean 保存処理の可否
      */
-    public function fetch($user) {
-        $this->authController->requestSpotifyTokenByRefreshToken($user->reflash_token);
+    public function fetch($spotifyId, $accessToken, $refreshToken) {
+        $this->authController->requestSpotifyTokenByRefreshToken($refreshToken);
 
         $timestamp = strtotime('-5 minutes');
-        $response = Http::withToken($user->access_token)->get('https://api.spotify.com/v1/me/player/recently-played', [
+        $response = Http::withToken($accessToken)->get('https://api.spotify.com/v1/me/player/recently-played', [
             'after' => $timestamp,
         ]);
 
@@ -34,7 +36,7 @@ class HistoryController extends Controller
         $items = $response->json()['items'];
         foreach ($items as $item) {
             $history = new History();
-            $history->user_spotify_id = $user->spotify_id;
+            $history->user_spotify_id = $spotifyId;
             $history->played_at = $item['played_at'];
             $history->track_spotify_id = $item['track']['id'];
             $history->save();
